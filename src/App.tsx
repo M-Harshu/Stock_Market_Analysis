f6ddf74ac9a6fcd8f49a0b5a7ed62b97907ab5f2
M-Harshu <mharshu2415@gmail.com>
2025-05-09 16:39:46 +0530

    Your message here, e.g., Updated Signup form styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,46 @@
-import { BrowserRouter as Router } from 'react-router-dom';
-import AppContent from './AppContent';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import TrendingStocks from './pages/TrendingStocks';
+import MarketNews from './pages/MarketNews';
+import StockScreener from './pages/StockScreener';
+import Learn from './pages/Learn';
+import About from './pages/About';
+import StockPredictions from './pages/StockPredictions';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import './index.css';
 
 function App() {
   return (
     <Router>
-      <AppContent />
+      <div className="flex flex-col min-h-screen bg-[#1E3A8A] text-black">
+        {/* Top Navigation Bar */}
+        <div className="bg-[#0D2818]">
+          <Navbar />
+        </div>
+
+        {/* Main Content Area */}
+        <main className="flex-grow">
+          <Routes>
+            {/* Initial Route */}
+            <Route path="/" element={<Signup />} /> {/* First route to sign up */}
+
+            {/* Other Routes */}
+            <Route path="/trending" element={<TrendingStocks />} />
+            <Route path="/news" element={<MarketNews />} />
+            <Route path="/screener" element={<StockScreener />} />
+            <Route path="/predictions" element={<StockPredictions />} />
+            <Route path="/learn" element={<Learn />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/login" element={<Login />} /> {/* Login page */}
+          </Routes>
+        </main>
+
+        {/* Bottom Footer */}
+        <Footer />
+      </div>
     </Router>
   );
 }
